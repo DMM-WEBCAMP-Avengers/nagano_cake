88b2c4f9f57af3dc8701bf556b94d2f04341470b
takeru0922 <moriai0922takeru@gmail.com>
2020-04-30 14:58:00 +0000

    modified

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
   belongs_to :order_histories
   belongs_to :destinations, dependent: :destroy
   belongs_to :cart_products, dependent: :destroy
