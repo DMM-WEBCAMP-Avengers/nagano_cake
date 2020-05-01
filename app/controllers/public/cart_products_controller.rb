6eb64355d607a4bb0cd93b93a7181b36edb9ec72
takeru0922 <moriai0922takeru@gmail.com>
2020-05-01 04:21:37 +0000

    generate-controller

---
+++ app/controllers/public/cart_products_controller.rb
@@ -0,0 +1,16 @@
+class Public::CartProductsController < ApplicationController
+  def create
+  end
+
+  def index
+  end
+
+  def update
+  end
+
+  def destroy_all
+  end
+
+  def destroy
+  end
+end
---
+++ app/helpers/public/cart_products_helper.rb
@@ -0,0 +1,2 @@
+module Public::CartProductsHelper
+end
