c255165104b1f75b888e0ccf3abc9fa7f9a82165
takeru0922 <moriai0922takeru@gmail.com>
2020-05-02 05:32:31 +0000

    仮作成admin/products/new

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,11 +1,15 @@
 class Admin::ProductsController < ApplicationController
   def new
+    @product = Product.new
   end
 
   def index
   end
 
   def create
+    @product = Product.new(product_params)
+    @product.save
+    redirect_to admin_product_path(@product.id)
   end
 
   def edit
@@ -15,6 +19,12 @@ class Admin::ProductsController < ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:image, :name, :introduction, :genre, :price, :validation)
   end
 
 end
