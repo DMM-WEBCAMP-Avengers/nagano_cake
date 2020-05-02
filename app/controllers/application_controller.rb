9d5fb55191ff90e178d03b0cc7ec7a524b61ed8e
utkazu <hiroshimazeal@gmail.com>
2020-05-02 00:58:52 +0000

    各種コメントを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+  #deviseのストロングパラメータ記述
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :kana_first_name, :kana_last_name, :postal_code, :address, :phone_number])
   end
