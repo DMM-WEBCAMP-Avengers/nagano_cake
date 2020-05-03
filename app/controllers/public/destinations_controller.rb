f6cf7d762acfc4968d2f8ea87816e05f52dbab2b
toshiaki suzuki <toshiaki1986@gmail.com>
2020-05-03 10:10:55 +0000

    OnTheWay

--- app/controllers/public/destinations_controller.rb
+++ app/controllers/public/destinations_controller.rb
@@ -6,11 +6,23 @@ class Public::DestinationsController < ApplicationController
   end
 
   def edit
+    @user =User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+      if
+       @user.update(user_params)
+       redirect_to destinations_path(@user.id)
+      else
+       render :edit
+      end
   end
 
   def destroy
+  end
+  private
+  def user_params
+    params.require(:user).permit(:postal_code, :address, :address)  #addressカラムがない
   end
 end
