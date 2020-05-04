a05d6dc474508b2b47121f430caf24a5f65dc1bd
toshiaki suzuki <toshiaki1986@gmail.com>
2020-05-04 09:24:50 +0000

    enroute

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,7 +1,11 @@
 class Public::ProductsController < ApplicationController
   def index
+    @products = Product.all.page(params[:page]).per(8)
+    @quantity = Product.count
   end
 
   def show
+    @product = Product.find(params[:id])
+    @ordered_product = Ordered_product.new
   end
 end
