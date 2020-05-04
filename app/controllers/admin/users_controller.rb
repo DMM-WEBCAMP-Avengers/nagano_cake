20bc59b9fa603dc25e9d71b25a3d1ece61a06dc0
takeru0922 <moriai0922takeru@gmail.com>
2020-05-04 10:29:39 +0000

    仮作成-会員一覧

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,21 +3,23 @@ class Admin::UsersController < ApplicationController
   end
 
   def index
+    @users = User.all
+    @users = User.page(params[:page]).per(10)
   end
 
   def edit
   end
 
   def show
-    @users = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def update
   end
 
-  private
+  # private
 
-  def user_params
-    params.require(:user).permit(:user_id, :validation, :first_name, :last_name, :kana_first_name, :kana_last_name, :postal_code, :address, :phone_number, :email)
-  end
+  # def user_params
+  #   params.require(:user).permit(:user_id, :validation, :first_name, :last_name, :kana_first_name, :kana_last_name, :postal_code, :address, :phone_number, :email)
+  # end
 end
