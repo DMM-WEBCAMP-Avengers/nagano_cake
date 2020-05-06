9c6a937d2d2615da82ddcdb145d6b2914495f238
utkazu <hiroshimazeal@gmail.com>
2020-05-06 18:35:14 +0900

    close #81 admin/orders#indexが遷移元のページによって表示を変えるよう修正

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,18 @@
 class Admin::OrdersController < ApplicationController
   def index
-    @order_histories = OrderHistory.page(params[:page]).per(10)
+    params[:prev_path] ? @path = params[:prev_path].permit! : @path = Rails.application.routes.recognize_path(request.referer)
+    if @path[:controller] == "admin/users"
+      case @path[:action]
+        when "top"
+          #最初の表示は行けるが、ページネーションで他のページに行くとエラーが出る。
+          @order_histories = OrderHistory.where(created_at: Time.zone.today.all_day).order(created_at: "DESC").page(params[:page]).per(10)
+        when "show"
+          @user_id = params[:user_id]
+          @order_histories = OrderHistory.where(user_id: params[:user_id]).order(created_at: "DESC").page(params[:page]).per(10)
+      end
+    else
+      @order_histories = OrderHistory.order(created_at: "DESC").page(params[:page]).per(10)
+    end
   end
 
   def show
