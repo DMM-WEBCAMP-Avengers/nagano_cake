f92b6369a1690ca63b8da21bf855f166e6fe8a6c
toshiaki suzuki <toshiaki1986@gmail.com>
2020-05-07 13:24:50 +0000

    finish

--- app/controllers/public/cart_products_controller.rb
+++ app/controllers/public/cart_products_controller.rb
@@ -1,5 +1,13 @@
 class Public::CartProductsController < ApplicationController
   def create
+    @cart_product = CartProduct.new(cart_product_params)
+    @cart_product.user_id = current_user.id
+    if @cart_product.save
+       redirect_to cart_products_path,success: '商品をカートに追加しました.'
+    else
+       @product = Product.find(@cart_product.product_id)
+       render 'public/products/show' #publicディレクトリのproductsフォルダのshowページへrender。(renderするとViewが乱れるけどOK)
+    end
   end
 
   def index
@@ -12,5 +20,10 @@ class Public::CartProductsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def cart_product_params
+    params.require(:cart_product).permit(:quantity, :product_id)
   end
 end
