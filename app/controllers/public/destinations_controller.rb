3374d8bc0cb97243c2fb1895f91973751a662d22
toshiaki suzuki <toshiaki1986@gmail.com>
2020-05-07 12:02:09 +0000

    Finish

--- app/controllers/public/destinations_controller.rb
+++ app/controllers/public/destinations_controller.rb
@@ -6,13 +6,13 @@ class Public::DestinationsController < ApplicationController
   end
 
   def edit
-    @user =User.find(params[:id])
+    @destination =Destination.find(params[:id])
   end
 
   def update
-    if
-      @user.update(current_user)
-      redirect_to destinations_path(@user.id)
+    @destination =Destination.find(params[:id])
+    if @destination.update(destination_params)
+       redirect_to destinations_path(@destination.id)
     else
        render :edit
       end
@@ -20,8 +20,9 @@ class Public::DestinationsController < ApplicationController
 
   def destroy
   end
+
   private
-  def user_params
-    params.require(:user).permit(:postal_code, :address, :address)  #addressカラムがない
+  def destination_params
+    params.require(:destination).permit(:postal_code, :address, :addressee)
   end
 end
