73b944b8ac67f77d07db89bad6ec98bf2e3157a1
takeru0922 <moriai0922takeru@gmail.com>
2020-05-08 04:19:51 +0000

    order_history関係controller未確認

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,6 +6,11 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirmation
+    @cart_products = CartProduct.where(user_id: current_user.id)
+    @order_histories = OrderHistory.where(user_id: current_user.id)
+    # @order_histories = current_user.order_histories.find(order_history_params)
+    # @order_histories = @cart_products.user_id.find(current_order_history)
+    @ordered_products =
   end
 
   def finish
@@ -18,5 +23,11 @@ class Public::OrdersController < ApplicationController
   def show
     @order_history = OrderHistory.find(params[:id])
     @ordered_products = @order_history.ordered_products
+  end
+
+  private
+
+  def order_history_params
+    params.require(:order_history).permit(:payment_option, :postage, :billing, :addressee, :postal_code, :address)
   end
 end
