ead9ca44a10a4a9cb3cc1e3584757b03612671e8
toshiaki suzuki <toshiaki1986@gmail.com>
2020-05-09 07:32:25 +0000

    フレンドリーフォワーディングの実装enroute

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :store_current_location, unless: :devise_controller?
+
   add_flash_types :success, :info, :warning, :danger
 
   protected
