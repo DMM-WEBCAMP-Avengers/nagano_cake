043103cf6f1026557229812e7ead4050007222d3
toshiaki suzuki <toshiaki1986@gmail.com>
2020-05-09 09:34:00 +0000

    商品詳細ページの遷移の修正（顧客と会員で分けた）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,10 +8,5 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :kana_first_name, :kana_last_name, :postal_code, :address, :phone_number])
   end
-  #フレンドリーフォワーディングの実装
-  def store_current_location
-    return if current_user
-    store_location_for(:user, request.url)
-  end
 
 end
