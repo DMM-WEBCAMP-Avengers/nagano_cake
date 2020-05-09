1c399b57e462cb6448bd5e263f7d83a0e7a867ab
toshiaki suzuki <toshiaki1986@gmail.com>
2020-05-09 03:53:12 +0000

    フレンドリーフォワーディングenroute

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :store_current_location, unless: :devise_controller?
   add_flash_types :success, :info, :warning, :danger
 
   protected
   #deviseのストロングパラメータ記述
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :kana_first_name, :kana_last_name, :postal_code, :address, :phone_number])
+  end
+  #フレンドリーフォワーディングの実装
+  def store_current_location
+    return if current_user
+    store_location_for(:user, request.url)
   end
 
 end
