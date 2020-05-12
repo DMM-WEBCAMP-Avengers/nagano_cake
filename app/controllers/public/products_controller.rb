037c9d99204d68ccd9685008835f8a606d4ca9a5
utkazu <hiroshimazeal@gmail.com>
2020-05-12 23:15:10 +0900

    トップ、商品一覧・詳細ページに検索サイドバーを設置

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -2,10 +2,12 @@ class Public::ProductsController < ApplicationController
   def index
     @products = Product.all.page(params[:page]).per(8)
     @quantity = Product.count
+    @genres = Genre.where(validation: true)
   end
 
   def show
     @product = Product.find(params[:id])
     @cart_product = CartProduct.new #Cart_product.new キャメルケースを利用
+    @genres = Genre.where(validation: true)
   end
 end
